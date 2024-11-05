0e9a43145c6fdbf0cacd416cd2c4398a337c9ea6
arnaudon <alexis.arnaudon@epfl.ch>
2024-11-05 15:56:08 +0100

    deps

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ spec.loader.exec_module(module)
 VERSION = module.VERSION
 
 reqs = [
-    "bluepy-configfile>=0.1.21",
-    "bluepy>=2.5.5",
     "bluepysnap>=3.0.1",
     "brainbuilder>=0.20.1",
     "click>=8",
@@ -32,7 +30,7 @@ reqs = [
     "pyglet>=1.5.20,<2",
     "pyquaternion>=0.9.5",
     "pyquaternion>=0.9.5",
-    "region_grower>=1.2.8",
+    #"region_grower>=1.2.8",
     "scipy>=1.13",
     "shapely>=2",
     "tqdm>=4.60",
